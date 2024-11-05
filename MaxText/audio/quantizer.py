5746ae6d5d6b7f752e904f07bcd3e2e607fdf2b3
Mddct <hamddct@gmail.com>
2024-11-05 21:29:05 +0800

    bestrq: part3: codebook metrics

--- MaxText/audio/quantizer.py
+++ MaxText/audio/quantizer.py
@@ -14,32 +14,28 @@ See the License for the specific language governing permissions and
 limitations under the License.
 """
 
-from sys import flags
 from typing import Any, Tuple
-import flax
 
+import common_types
 import flax.linen as nn
 import jax
 import jax.numpy as jnp
-import MaxText.common_types
-from MaxText.layers.attentions import NdInitializer
-from MaxText.layers.gpt3 import DenseGeneral
-from MaxText.layers.initializers import nd_dense_init
+from layers.attentions import NdInitializer
+from layers.gpt3 import DenseGeneral
+from layers.initializers import nd_dense_init
 
-Config = MaxText.common_types.DType
+Config = common_types.DType
 
 
-def compute_code_histogram(onehots: jax.Array, paddings: jax.Array):
+def compute_code_histogram(onehots: jax.Array):
     """Computes histograms of the quantized codes over the codebook vocabulary.
 
     Args:
-        onehots: Quantized onehots. Array of shape [..., num_codebooks, codebook_size].
-        paddings: paddings of the quantized codes. Array of shape [...].
+        onehots: Quantized onehots. Array of shape [..., num_groups, codebook_size].
 
     Returns:
-        Histogram of the quantized codes of shape [num_codebooks, codebook_size].
+        Histogram of the quantized codes of shape [num_groups, codebook_size].
     """
-    onehots = onehots * (1 - paddings)[..., None, None]
     # [num_codebooks, codebook_size].
     histogram = jnp.sum(onehots, axis=tuple(range(onehots.ndim - 2)))
     return histogram
@@ -47,23 +43,23 @@ def compute_code_histogram(onehots: jax.Array, paddings: jax.Array):
 
 def compute_code_pplx(onehots: jax.Array, paddings: jax.Array):
     """Computes pplx and entropy of the quantized codes distribution."""
-    histogram = compute_code_histogram(onehots, paddings)
+    histogram = compute_code_histogram(onehots)
     normalizer = jnp.sum(1 - paddings)
-    # [num_codebooks, codebook_size].
+    # [num_groups, codebook_size].
     probs = histogram / jnp.maximum(normalizer, 1.0)
     log_probs = jnp.log(jnp.maximum(1.0e-30, probs))
-    # [num_codebooks].
+    # [num_groups].
     sum_plogp = jnp.sum(log_probs * probs, axis=-1)
     pplx = jnp.mean(jnp.exp(-sum_plogp))
     entropy = jnp.log(pplx)
     return pplx, entropy
 
 
-def compute_code_coverage(onehots: jax.Array, paddings: jax.Array):
+def compute_code_coverage(onehots: jax.Array):
     """Computes codebook coverage."""
     codebook_size = onehots.shape[-1]
-    # [num_codebooks, codebook_size].
-    histogram = compute_code_histogram(onehots, paddings)
+    # [num_groups, codebook_size].
+    histogram = compute_code_histogram(onehots)
     avg_num_covered_words = jnp.mean(
         jnp.sum((histogram > 0).astype(jnp.float32), axis=-1))
     return avg_num_covered_words / codebook_size
@@ -72,18 +68,48 @@ def compute_code_coverage(onehots: jax.Array, paddings: jax.Array):
 def quantize_vector(
         latent: jax.Array,
         codebook: jax.Array) -> Tuple[jax.Array, jax.Array, jax.Array]:
-    """ https://github.com/wenet-e2e/wenet/blob/main/wenet/ssl/bestrq/bestrq_model.py
-    """
+    """Vector quantization.
+    https://github.com/wenet-e2e/wenet/blob/main/wenet/ssl/bestrq/bestrq_model.py
+    Symbols in comments:
+    B: batch_size.
+    D: latent_dim.
+    C: num_latent_classes per group
+    G: num of codebook groups.
 
-    v, g, h = codebook.shape
-    # Calculate distance
-    distance = (jnp.sum(latent**2, axis=-1, keepdims=True) -
-                2 * jnp.einsum("...gh,vgh->...gv", latent, codebook) +
-                jnp.sum(codebook**2, axis=-1).T)
+    Args:
+        latent:   [B, D]
+        codebook: [C, G, D // G]
+
+    Returns:
+        (quantized, codes, onehot).
+        - quantized: [B, D]
+        - codes:     [B, G]
+        - onehot:    [B, G, C]
+    """
+    assert len(codebook.shape) == 3
+    b, d = latent.shape
+    c, g = codebook.shape[:2]
+    assert d % g == 0
+
+    latent = jnp.reshape(latent, [b, g, d // g])
+
+    # [B, G, C]
+    distance = (
+        # [b, g, 1]
+        jnp.sum(latent**2, -1, keepdims=True) -
+        # [b, g, c]
+        2 * jnp.einsum('bgd,cgd->bgc', latent, codebook) +
+        # [1, g, c]
+        jnp.sum(jnp.transpose(codebook, [2, 1, 0])**2, 0, keepdims=True))
+
+    # [B, G]
     codes = jnp.argmin(distance, axis=-1)
-    onehot = jax.nn.one_hot(codes, v, axis=-1)
-    quantized = jnp.einsum("...gv,vgh->...gh", onehot, codebook)
-    return quantized, codes, onehot
+
+    # [B, G, C]
+    one_hot = jax.nn.one_hot(codes, c, axis=-1, dtype=jnp.float32)
+    quantized = jnp.einsum('bgc,cgd->bgd', one_hot, codebook)
+    quantized = jnp.reshape(quantized, [b, d])
+    return quantized, codes, one_hot
 
 
 def _l2_normalize(x: jax.Array,
@@ -108,13 +134,12 @@ class RandomVectorQuantizer(nn.Module):
     normalize_latent_vector: bool = False
     normalize_latent_per_group: bool = True
 
-
     # xavier init
     random_proj_init: NdInitializer = nd_dense_init(
         1.0, mode='fan_avg', distribution='truncated_normal')
 
     # noram init
-    random_codebook_init: NdInitializer = nn.initializers.normal(stddev=1.0),
+    random_codebook_init: NdInitializer = nn.initializers.normal(stddev=1.0)
 
     def setup(self):
         # Initialize the random projection layer
@@ -131,42 +156,54 @@ class RandomVectorQuantizer(nn.Module):
             matmul_precision=self.config.matmul_precision,
         )
         # Initialize and freeze the codebook
-        kernel_axes=('codebooks', 'code_gorups', 'codebook_dim')
+        kernel_axes = ('codebooks', 'code_gorups', 'codebook_dim')
         self.codebook = self.param(
             'codebooks',
-            nn.with_logical_partitioning(self.random_codebook_init, kernel_axes),
+            nn.with_logical_partitioning(self.random_codebook_init,
+                                         kernel_axes),
             (self.num_codebooks, self.num_groups, self.codebook_dim),
             self.weight_dtype,
         )
-        # Normalize codebook if enabled
-        if self.normalize_codebook:
-            self.codebook = self.codebook / (
-                jnp.linalg.norm(self.codebook, axis=-1, keepdims=True) + 1e-12)
 
     def __call__(self, inputs, paddings):
+
+        # Get codebook
+        codebook = self.codebook
+        if self.normalize_codebook:
+            codebook = _l2_normalize(self.codebook, axis=-1)
         # Compute random projection
-        inputs= jnp.asarray(inputs, self.dtype)
+        inputs = jnp.asarray(inputs, self.dtype)
         inputs = self.rand_proj(
             inputs)  # [batch_size, seq_len, num_codebooks * codebook_dim]
+
+        # TODO: logical constraint
 
         if self.normalize_latent_vector and not self.normalize_latent_per_group:
             inputs = _l2_normalize(inputs, -1)
 
         # Reshape for group-wise quantization
         b, l, d = inputs.shape
-        proj_by_group = inputs.reshape(b, l, self.num_groups, d // self.num_groups)
-
-
+        proj_by_group = inputs.reshape(b * l * d)
         if self.normalize_latent_vector and self.normalize_latent_per_group:
             proj_by_group = _l2_normalize(proj_by_group, -1)
 
         # Quantize using vector quantization
-        q, codes, onehot = quantize_vector(proj_by_group, self.codebook)
+        q, codes, onehot = quantize_vector(proj_by_group, codebook)
+        # TODO: logical constraint
+
         q = q.reshape(b, l, d)
+        codes = codes.reshape(b, l, self.num_groups)
+        onehot = onehot.reshape(b, l, self.num_groups, self.num_codebooks)
 
         # Apply paddings
-        q = q * (1 - paddings)[..., None, None]
-        onehots = onehot * (1 - paddings)[..., None]
+        q = q * (1 - paddings)[..., None]
+        codes = codes * (1 - paddings)[..., None]
+        onehots = onehot * (1 - paddings)[..., None, None]
+
+        pplx, entropy = compute_code_pplx(onehot, paddings)
+        self.sow('codebook', 'coverage', compute_code_coverage(onehot))
+        self.sow('codebook', 'pplx', pplx)
+        self.sow('codebook', 'entropy', entropy)
 
         return {
             "ids": jax.lax.stop_gradient(codes),
